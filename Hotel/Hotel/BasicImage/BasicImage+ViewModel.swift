312a3751c46fb1eac1c43f09ed8596b539fd1eb8
Kamil701 <90286444+Kamil701@users.noreply.github.com>
2023-09-19 23:34:16 +0500

    create viewModels

---
+++ Hotel/Hotel/BasicImage/BasicImage+ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BasicImage+ViewModel.swift
+//  Hotel
+//
+//  Created by Kamil Vakhobov on 19.09.23.
+//
+
+import Foundation
---
+++ Hotel/Hotel/BasicLabel /BasicLabel+ViewModel .swift
@@ -0,0 +1,8 @@
+//
+//  BasicLabel+ViewModel .swift
+//  Hotel
+//
+//  Created by Kamil Vakhobov on 19.09.23.
+//
+
+import Foundation
